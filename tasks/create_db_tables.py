2c6b54638445f7292d1d867665b31388b06b237b
GeekNekoS <anastasiyanesterenko@icloud.com>
2023-10-19 23:41:50 +0500

    Переформиравала запрос на подключение к БД, смена музыкальной темы

--- tasks/create_db_tables.py
+++ tasks/create_db_tables.py
@@ -1,26 +1,20 @@
 import psycopg2
 
+
 try:
-    connection = psycopg2.connect('postgresql://postgres:1@localhost:5432/schedules')
-    cursor = connection.cursor()
-
-    cursor.execute("""
-        CREATE TABLE Customers (
-            id INTEGER,
-            name VARCHAR,
-            surname VARCHAR,
-            birthdate DATE,
-            PRIMARY KEY (id)
-        );
-    """)
-
-    connection.commit()
-
-    cursor.close()
-    connection.close()
-    
+    with psycopg2.connect('postgresql://postgres:1@localhost:5432/schedules') as connection:
+        cursor = connection.cursor()
+        cursor.execute("""
+            CREATE TABLE Modules (
+                id INTEGER,
+                itis VARCHAR,
+                history VARCHAR,
+                math VARCHAR,
+                ORG VARCHAR,
+                physics VARCHAR,
+                english VARCHAR,
+                PRIMARY KEY (id)
+            );
+        """)
 except Exception as ex:
-    print('Can`t establish connection to database')
-    print(ex)
-
-# доделать
+    print(f"Can`t establish connection to database: {ex}\n")
