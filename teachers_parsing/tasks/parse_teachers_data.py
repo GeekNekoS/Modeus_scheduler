3553a9a51eefba3981cc69b488f863d7e379c6f4
GeekNekoS <anastasiyanesterenko@icloud.com>
2023-11-22 15:44:16 +0500

    Передаю наработки Тимофею

--- teachers_parsing/tasks/parse_teachers_data.py
+++ teachers_parsing/tasks/parse_teachers_data.py
@@ -18,25 +18,25 @@ def parse_teachers_data():
     # Working with modeus personal pages
     modeus_page = TeachersParsing(driver)
 
-    pages = modeus_page.get_pages()
-    for page in pages:
-        print(page)
-
     modeus_page.create_teachers_table()
     modeus_page.go_to_somewhere()
-    teachers_cards = modeus_page.get_teachers_cards()
-    for teacher_card in teachers_cards:
-        teacher_data = teacher_card.text.split("\n")
-        teacher_name = teacher_data[0]
-        teacher_phone = "Нет данных"
-        teacher_email = "Нет данных"
-        for item in teacher_data:
-            if "Телефон: " in item:
-                teacher_phone = item.replace("Телефон: ", "")
-            if "Электронная почта: " in item:
-                teacher_email = item.replace("Электронная почта: ", "")
 
-        print(teacher_name, teacher_phone, teacher_email, "\n")
+    pages = modeus_page.get_pages()
+    for page in pages:
+        modeus_page.get_connect(page)
+        teachers_cards = modeus_page.get_teachers_cards()
+        for teacher_card in teachers_cards:
+            teacher_data = teacher_card.text.split("\n")
+            teacher_name = teacher_data[0]
+            teacher_phone = "Нет данных"
+            teacher_email = "Нет данных"
+            for item in teacher_data:
+                if "Телефон: " in item:
+                    teacher_phone = item.replace("Телефон: ", "")
+                if "Электронная почта: " in item:
+                    teacher_email = item.replace("Электронная почта: ", "")
+
+            print(teacher_name, teacher_phone, teacher_email, "\n")
 
     time.sleep(1)
 
