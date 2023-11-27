75fdcd20454c16c08c17184e09c31a673477014f
demirtry <dimakol2005@mail.ru>
2023-11-28 00:47:14 +0500

    Создание "Многоступенчатого" бота, в котором можно последовательно переходить от одного раздела к другому

--- telegram_bot/markups.py
+++ telegram_bot/markups.py
@@ -5,34 +5,31 @@ def start_markup():
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
     btn1 = types.KeyboardButton('Составить расписание')
-    # btn2 = types.KeyboardButton('Привет')
-    btn3 = types.KeyboardButton('Отзывы')
-    # btn4 = types.KeyboardButton('Техника безопасности')
+    btn2 = types.KeyboardButton('Отзывы')
     markup.add(btn1)
-    markup.add(btn3)
+    markup.add(btn2)
 
     return markup
 
 
-def safety_markup():
+def second_step_reviews_markup():
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
-    btn1 = types.KeyboardButton('Смотреть правила')
-    btn2 = types.KeyboardButton('Мой прогресс')
-    btn3 = types.KeyboardButton('Назад в меню')
-
+    btn1 = types.KeyboardButton('Смотреть отзывы')
+    btn2 = types.KeyboardButton('Оставить отзыв')
+    btn3 = types.KeyboardButton('Назад')
     markup.add(btn1, btn2)
     markup.add(btn3)
 
     return markup
 
 
-def reviews_markup():
+def first_step_reviews_markup():
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
-    btn1 = types.KeyboardButton('Смотреть отзывы')
-    btn2 = types.KeyboardButton('Оставить отзыв')
-    btn3 = types.KeyboardButton('Назад в меню')
+    btn1 = types.KeyboardButton('Текстовые отзывы')
+    btn2 = types.KeyboardButton('Рейтинговые отзывы')
+    btn3 = types.KeyboardButton('Назад')
     markup.add(btn1, btn2)
     markup.add(btn3)
 
@@ -42,7 +39,7 @@ def reviews_markup():
 def back_to_start_markup():
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
-    btn1 = types.KeyboardButton('Назад в меню')
+    btn1 = types.KeyboardButton('Назад')
     markup.add(btn1)
 
     return markup
@@ -53,8 +50,20 @@ def modeus_markup():
 
     btn1 = types.KeyboardButton('Приступить к созданию')
     btn2 = types.KeyboardButton('Выйти из аккаунта')
-    btn3 = types.KeyboardButton('Назад в меню')
+    btn3 = types.KeyboardButton('Назад')
     markup.add(btn1, btn2)
     markup.add(btn3)
+
+    return markup
+
+
+def delete_text_review_markup():
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+    btn1 = types.KeyboardButton('Удалить этот отзыв')
+    btn2 = types.KeyboardButton('Назад')
+
+    markup.add(btn1)
+    markup.add(btn2)
 
     return markup
